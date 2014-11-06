85a6a894004a59b0c5b71a68165523532be0279b
Pedro Costa <dev@iampfac.com>
2014-11-06 18:02:48 +0000

    Adds spring and boilerplate for the API

---
+++ src/main/java/com/iampfac/suchread/api/config/MvcConfig.java
@@ -0,0 +1,11 @@
+package com.iampfac.suchread.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+}
