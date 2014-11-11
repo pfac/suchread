705b817b1f0892ae7587066475b3597ed58d1a3d
Pedro Costa <dev@iampfac.com>
2014-11-11 18:30:16 +0000

    Configures Spring Security with LDAP
    Also:
    * Adds classes to test the API as REST JSON

--- src/main/java/com/iampfac/suchread/api/HomeController.java
+++ src/main/java/com/iampfac/suchread/api/HomeController.java
@@ -1,5 +1,7 @@
 package com.iampfac.suchread.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	public HomeController() {
+		logger.warn("HomeController built");
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String getIndex() {
-		return "Hello world!";
+	public Greeting getIndex() {
+		return new Greeting();
 	}
 }
